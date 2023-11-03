98424d83757b82fa6e2e80593410b85cf17f0d07
DruidPro <hataaiee@gmail.com>
2023-11-04 01:34:01 +0500

    add npm and server test func

---
+++ public/script.js
@@ -0,0 +1,58 @@
+// DOM
+const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
+
+const user = {
+  likedAnime: []
+};
+const serializedData = JSON.stringify(user);
+
+fetch('https://api.jikan.moe/v4/top/anime')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const dataDisplay = data.data.map((elem) => {
+      return { id: elem.mal_id, image: elem.images.jpg.image_url };
+    });
+      appendNewCard();
+
+    function getRandomImageData() {
+      const randomIndex = Math.floor(Math.random() * dataDisplay.length);
+      const randomImage = dataDisplay[randomIndex];
+      return { imageUrl: randomImage.image, animeId: randomImage.id };
+    }
+
+    function appendNewCard() {
+      const { animeId, imageUrl } = getRandomImageData();
+      const card = new Card({
+        imageUrl: imageUrl,
+        onDismiss: appendNewCard,
+        onLike: () => {
+          like.style.animationPlayState = 'running';
+          like.classList.toggle('trigger');
+          user.likedAnime.push({ id: animeId, image: imageUrl});
+          socket.emit('sendArray', user);
+          console.log(user.likedAnime)
+        },
+        onDislike: () => {
+          dislike.style.animationPlayState = 'running';
+          dislike.classList.toggle('trigger');
+        },
+      });
+      swiper.append(card.element);
+
+      console.log(user.likedAnime);
+      const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+      cards.forEach((card, index) => {
+        card.style.setProperty('--i', index);
+      });
+    }
+  })
+  .catch((error) => {
+    console.error('Fetch error:', error);
+  });
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+ res.sendFile(__dirname + "/templates/index.html");
+});
+
+const connectedUsers = {};
+
+io.on("connection", (socket) => {
+ console.log("Пользователь подключился");
+
+ const randomNickname = generateRandomNickname();
+
+ const user = {
+  socket: socket,
+  nickname: randomNickname,
+  likedAnime: [],
+ };
+
+ connectedUsers[socket.id] = user;
+
+ const usernames = Object.values(connectedUsers).map((u) => u.nickname);
+ io.emit("userList", usernames);
+
+ socket.on("sendArray", (serializedData) => {
+  connectedUsers[socket.id].likedAnime = serializedData.likedAnime;
+
+  compareLikedAnime(connectedUsers[socket.id].likedAnime);
+ });
+
+ socket.on("disconnect", () => {
+  delete connectedUsers[socket.id];
+
+  const usernames = Object.values(connectedUsers).map((u) => u.nickname);
+  io.emit("userList", usernames);
+ });
+
+
+ function compareLikedAnime(data) {
+  Object.values(connectedUsers).forEach((otherUser) => {
+   let isMatchFound = false;
+
+   otherUser.likedAnime.some((anime1) => {
+    return user.likedAnime.some((anime2) => {
+     if (anime1.id === anime2.id) {
+      console.log(`Аниме с id ${user.nickname} совпало:`);
+      console.log(`Изображение: ${anime1.image}`);
+      user.socket.emit("matchingAnime", {
+       nickname: user.nickname,
+       image: anime1.image,
+       name: anime1.name,
+      });
+      isMatchFound = true;
+
+      return true; // Stop iterating further
+     }
+    });
+   });
+
+   if (!isMatchFound) {
+    console.log("No matching anime found.");
+   }
+  });
+  s;
+ }
+});
+
+server.listen(port, () => {
+ console.log("Listening app at http://localhost:" + port);
+});
+
+function generateRandomNickname() {
+ const adjectives = ["Happy", "Silly", "Clever", "Funny", "Adventurous"];
+ const nouns = ["Cat", "Dog", "Penguin", "Elephant", "Lion"];
+
+ const randomAdjective =
+  adjectives[Math.floor(Math.random() * adjectives.length)];
+ const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+
+ return randomAdjective + randomNoun;
+}
